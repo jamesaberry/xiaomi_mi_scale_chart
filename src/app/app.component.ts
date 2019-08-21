7c216a36a15a10381f454403b4f1ad322f33711d
jamesaberry <jamesaberry@users.noreply.github.com>
2019-08-21 12:15:09 -0400

    Working api calls and graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,68 @@
 import { Component,OnInit } from '@angular/core';
-import {Chart} from 'chart.js';
+import { Chart } from 'chart.js';
+import { freeApiService } from '../services/freeapi.service';
+import { ScaleEntry } from './classes/ScaleEntry';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(private _freeApiService: freeApiService) {
+  }
+  
   title = 'Mi Scale Weight Chart';
   LineChart=[];
+  scaleEntries:ScaleEntry[];
+  weightData=[];
+  dateData=[];
 
-  ngOnInit()
-  {
-    this.LineChart = new Chart('lineChart', {
-        type: 'line',
-        data: {
-            labels: ["Jan", "Feb", "March", "April"],
-            datasets: [{
-                label: 'Weight in lbs',
-                data: [293.10, 294.10, 295.20, 292.40],
-                fill:false,
-                lineTension:0.2,
-                borderColor:"red",
-                borderWidth: 1
-            }]
-        }, 
-        options: {
-        title:{
-            text:"Line Chart",
-            display:true
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:false
-                }
-            }]
-        }
-        }
+  async createChart(weightData, dateData) {
+    await this._freeApiService.getcomments().subscribe(data => {
+        this.scaleEntries = data;
+        this.scaleEntries.forEach(function (item, index) {
+            weightData.push(parseFloat(item.weight));
+            var date = new Date((parseFloat(item.timestamp) * 1000));
+            dateData.push(date.getFullYear() + "-" 
+                + (date.getMonth() + 1) + "-"
+                + date.getDate() + " "
+                + date.getHours()+ ":"
+                + date.getMinutes() + ":"
+                + date.getSeconds());
+          });
+          this.LineChart = new Chart('lineChart', {
+            type: 'line',
+            data: {
+                labels: this.dateData,
+                datasets: [{
+                    label: 'Weight in lbs',
+                    data: this.weightData,
+                    fill:false,
+                    lineTension:0.2,
+                    borderColor:"red",
+                    borderWidth: 1
+                }]
+            }, 
+            options: {
+            title:{
+                text:"Line Chart",
+                display:true
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:false
+                    }
+                }]
+            }
+            }
+        });
     });
+  }
+
+  async ngOnInit()
+  {
+    this.createChart(this.weightData, this.dateData);
   }
 }
---
+++ src/app/classes/ScaleEntry.ts
@@ -0,0 +1,8 @@
+export class ScaleEntry {
+    user: string;
+    weight: string;
+    bmi: string;
+    basal_metabolism: string;
+    visceral_fat: string;
+    timestamp: string;
+}
